bf87b3918d9d1fe9cba9b0bd52a3e1de54b8db6a
DrozdovDan <daniil.drozdovjr@gmail.com>
2023-09-13 21:40:40 +0300

    added log

--- DB_Scripts.py
+++ DB_Scripts.py
@@ -1,25 +1,30 @@
 import sqlite3 as sql
+import logging
 
 
 def add_info(calc, res):
+    logging.basicConfig(level=logging.INFO, filename='py_log.log', filemode='w')
     try:
         sqlite_connection = sql.connect('CalculatorDB.db')
+        logging.info('Connection to sqlite opened')
         execution = 'INSERT INTO History (Calculation, Result) VALUES (?, ?)'
         cursor = sqlite_connection.cursor()
         cursor.execute(execution, (calc, res))
         sqlite_connection.commit()
         cursor.close()
     except sql.Error as error:
-        print('Fail to connect to sqlite', error)
+        logging.error('Fail to connect to sqlite', error)
     finally:
         if (sqlite_connection):
             sqlite_connection.close()
-            print('Connection to sqlite closed')
+            logging.info('Connection to sqlite closed')
 
 
 def load_info():
+    logging.basicConfig(level=logging.INFO, filename='py_log.log', filemode='w')
     try:
         sqlite_connection = sql.connect('CalculatorDB.db')
+        logging.info('Connection to sqlite opened')
         execution = 'SELECT * FROM History'
         cursor = sqlite_connection.cursor()
         cursor.execute(execution)
@@ -28,8 +33,8 @@ def load_info():
         cursor.close()
         return rows
     except sql.Error as error:
-        print('Fail to connect to sqlite', error)
+        logging.error('Fail to connect to sqlite', error)
     finally:
         if (sqlite_connection):
             sqlite_connection.close()
-            print('Connection to sqlite closed')
+            logging.info('Connection to sqlite closed')
