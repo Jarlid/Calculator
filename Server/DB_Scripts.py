a0b337ca888679c3df53ddcf7a47a3c73e9e99f8
DrozdovDan <daniil.drozdovjr@gmail.com>
2023-09-14 11:31:54 +0300

    Put DB into a folder 'server'

--- Server/DB_Scripts.py
+++ Server/DB_Scripts.py
@@ -1,30 +1,31 @@
 import sqlite3 as sql
 import logging
+from datetime import datetime
 
 
 def add_info(calc, res):
     logging.basicConfig(level=logging.INFO, filename='py_log.log', filemode='w')
     try:
-        sqlite_connection = sql.connect('CalculatorDB.db')
-        logging.info('Connection to sqlite opened')
+        sqlite_connection = sql.connect('Calculator_DB.db', )
+        logging.info('Connection to sqlite opened' + datetime.now().strftime(' %H:%M:%S'))
         execution = 'INSERT INTO History (Calculation, Result) VALUES (?, ?)'
         cursor = sqlite_connection.cursor()
         cursor.execute(execution, (calc, res))
         sqlite_connection.commit()
         cursor.close()
     except sql.Error as error:
-        logging.error('Fail to connect to sqlite', error)
+        logging.error('Fail to connect to sqlite' + error.sqlite_errorname + datetime.now().strftime(' %H:%M:%S'))
     finally:
         if (sqlite_connection):
             sqlite_connection.close()
-            logging.info('Connection to sqlite closed')
+            logging.info('Connection to sqlite closed' + datetime.now().strftime(' %H:%M:%S'))
 
 
 def load_info():
     logging.basicConfig(level=logging.INFO, filename='py_log.log', filemode='w')
     try:
-        sqlite_connection = sql.connect('CalculatorDB.db')
-        logging.info('Connection to sqlite opened')
+        sqlite_connection = sql.connect('Calculator_DB.db')
+        logging.info('Connection to sqlite opened' + datetime.now().strftime(' %H:%M:%S'))
         execution = 'SELECT * FROM History'
         cursor = sqlite_connection.cursor()
         cursor.execute(execution)
@@ -33,8 +34,8 @@ def load_info():
         cursor.close()
         return rows
     except sql.Error as error:
-        logging.error('Fail to connect to sqlite', error)
+        logging.error('Fail to connect to sqlite' + error.sqlite_errorname + datetime.now().strftime(' %H:%M:%S'))
     finally:
         if (sqlite_connection):
             sqlite_connection.close()
-            logging.info('Connection to sqlite closed')
+            logging.info('Connection to sqlite closed' + datetime.now().strftime(' %H:%M:%S'))
