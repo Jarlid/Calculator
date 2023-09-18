eebe8754a0b4ad99b53f839cd3dcf9a3b3f63537
Jarlid <korkiniostudymail@gmail.com>
2023-09-18 12:48:46 +0300

    Добавлена оболочка для поддерки БД.

--- server/server.py
+++ server/server.py
@@ -4,22 +4,41 @@ from config import DEFAULT_ADDRESS, MAX_LEN, ENCODING
 from calculator import Calculator, CalculatorException
 
 
+class DummyException(Exception):  # TODO: удалить после добавления БД.
+    pass
+
+
+def execute_prompt(prompt: str, calculator: Calculator = Calculator()) -> str:
+    try:
+        if prompt == '#':
+            # TODO: достать историю из БД.
+            # load_info()
+            return "#"
+
+        result = calculator.calculate(prompt)
+        # TODO: отправить в БД.
+        # add_info(prompt, result)
+        return str(result)
+
+    except CalculatorException as e:
+        return '#' + str(e)
+
+    except DummyException:  # TODO: заменить DummyException на Exception от БД.
+        return '#Database error occurred.'
+
+
 def serve(bind=DEFAULT_ADDRESS):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(bind)
-
-    calculator = Calculator()
 
     while True:
         server.listen()
         connection, _ = server.accept()
 
         prompt = connection.recv(MAX_LEN).decode(ENCODING)
-        try:
-            result = calculator.calculate(prompt)
-            connection.send(str(result).encode(ENCODING))
-        except CalculatorException as e:
-            connection.send(('#' + str(e)).encode(ENCODING))
+        result = execute_prompt(prompt)
+
+        connection.send(result.encode(ENCODING))
         connection.close()
 
 
