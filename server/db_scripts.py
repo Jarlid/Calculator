60cc46459222c5f4e1bddd0d54512f0d41cc0be4
DrozdovDan <daniil.drozdovjr@gmail.com>
2023-09-19 20:29:59 +0300

    Fix code style

--- server/db_scripts.py
+++ server/db_scripts.py
@@ -5,7 +5,7 @@ from db_config import DB_NAME, TABLE_NAME, LOG_FILE_NAME, BASIC_LEVEL_OF_LOGGING
     CONNECTION_TO_SQLITE_CLOSED, CONNECTION_TO_SQLITE_OPENED, TABLE_SQLITE_CREATED, SUCC_SELECT, SUCC_INSERT
 
 
-class db_exception(Exception):
+class DBException(Exception):
     """Throwing errors with db outside"""
     pass
 
@@ -26,7 +26,7 @@ def add_info(calc, res):
         if (sqlite_connection):
             sqlite_connection.close()
             logging.info(CONNECTION_TO_SQLITE_CLOSED.format(time=datetime.utcnow().strftime('%H:%M:%S')))
-        raise db_exception("Can't insert into the db, " + repr(error))
+        raise DBException("Can't insert into the db, " + repr(error))
     finally:
         if (sqlite_connection):
             sqlite_connection.close()
@@ -53,7 +53,7 @@ def load_info() -> str:
         if (sqlite_connection):
             sqlite_connection.close()
             logging.info(CONNECTION_TO_SQLITE_CLOSED.format(time=datetime.utcnow().strftime('%H:%M:%S')))
-        raise db_exception("Can't load from the db, " + repr(error))
+        raise DBException("Can't load from the db, " + repr(error))
     finally:
         if (sqlite_connection):
             sqlite_connection.close()
@@ -80,7 +80,7 @@ def create_db():
         if (sqlite_connection):
             sqlite_connection.close()
             logging.info(CONNECTION_TO_SQLITE_CLOSED.format(time=datetime.utcnow().strftime('%H:%M:%S')))
-        raise db_exception("Can't create a db, " + repr(error))
+        raise DBException("Can't create a db, " + repr(error))
     finally:
         if (sqlite_connection):
             sqlite_connection.close()
